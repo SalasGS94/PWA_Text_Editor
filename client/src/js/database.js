c87c8a3df3628666ac0d27f3c8d1b9814320957f
unknown <gergasa94@gmail.com>
2023-08-04 22:39:04 -0600

    package.json updated

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,23 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: FINISHED Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, todo: content });
+  const request = store.put({ id: 1, jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: FINISHED Add logic for a method that gets all the content from the database
-export const getDb = async (id) => {
+export const getDb = async () => {
   console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.get(id);
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
   return result;
